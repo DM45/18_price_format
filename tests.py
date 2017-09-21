fb5c47cbfa948aff66272d0dab757444e94725b3
DM45 <mda.tomsk@gmail.com>
2017-09-22 00:16:49 +0700

    Решение задачи

--- tests.py
+++ tests.py
@@ -1,1 +1,17 @@
-# TODO place your tests here
+import unittest
+
+from format_price import validation_price
+
+
+class FormatPriceTestCase(unittest.TestCase):
+    def test_negetive_price(self):
+        price = validation_price(-1)
+        self.assertIsNotNone(price)
+
+    def test_cant_be_floated(self):
+        price = validation_price('abc')
+        self.assertIsNotNone(price)
+
+
+if __name__ == '__main__':
+    unittest.main()
