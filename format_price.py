78148638b02b4009bd2e94b99e81597936e08e87
DM45 <mda.tomsk@gmail.com>
2017-09-22 23:00:48 +0700

    Решение задачи

--- format_price.py
+++ format_price.py
@@ -5,46 +5,41 @@ def validation_price(price):
 	val_for_zero_check = 0
 	try:
 		price = float(price)
-	except:
-		return 'Something wrong with number!'
+	except (TypeError, ValueError):
+		return 'Wrong format of number!'
 	if price < val_for_zero_check:
 		return 'Price cant be negative!'
 
 
 def format_price(input_price):
-    mod_1, mod_2, mod_3 = 1, 2, 3
-    fract_part_price, int_part_price = math.modf(float(price))
-    fract_part_price = round(fract_part_price, mod_2)
+    mod_0, mod_1, mod_2, mod_3, mod_4 = 0, 1, 2, 3, 4
+    try:
+        int_part_price, fract_part_price = input_price.split('.')
+        fract_part_price = '{}.{}'.format(mod_0, fract_part_price)
+        fract_part_price = round(float(fract_part_price), mod_2)
+    except ValueError:
+        int_part_price = input_price
+        fract_part_price = 0
+    len_list = len(int_part_price) + len(int_part_price) // mod_3
+    int_part_price = list(int_part_price)
+    int_part_price.reverse()
+    for elem in range(len_list):
+        if (not (elem + mod_1) % mod_4) and elem != len_list - mod_1:
+            int_part_price.insert(elem, ' ') 
+    int_part_price.reverse()
     formated_price = ''
-    int_part_price = str(int(int_part_price))
-    len_price = len(int_part_price)
-    int_part_price = int_part_price[::-mod_1]
-    for number in range(mod_1, len_price + mod_1):
-        if not number % mod_3:
-        	if formated_price:
-        		formated_price = '{} {}'.format(
-        			    formated_price, int_part_price[number - mod_3:number])
-        	else:
-        		formated_price = int_part_price[number - mod_3:number]
-    if len_price % mod_3:
-    	if not (len_price - mod_1) % mod_3:
-            formated_price = '{} {}'.format(
-            	    formated_price, int_part_price[number - mod_1:number])
-    	else:
-    		formated_price = '{} {}'.format(
-    			    formated_price, int_part_price[number - mod_2:number])
-    formated_price = formated_price[::-mod_1]
+    for elem in int_part_price:
+        formated_price = '{}{}'.format(formated_price, elem)
     if fract_part_price:
-    	zero_arg, fract_arg = str(fract_part_price).split('.')
-    	formated_price = '{}.{}'.format(formated_price, fract_arg)
+        formated_price = '{}{}'.format(formated_price, fract_part_price)
     return formated_price
 
 
 if __name__ == '__main__':
     input_price = input('Enter price: ')
-    validation_price = validation_price(price)
+    validation_price = validation_price(input_price)
     if not validation_price:
-        format_price = format_price(price)
+        format_price = format_price(input_price)
         print(format_price)
     else:
         print(validation_price)
