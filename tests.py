ff3f4b3acf62355fce7e88f8f8834fad5ba0d3a7
DM45 <mda.tomsk@gmail.com>
2017-09-22 23:02:35 +0700

    Добавление тестов

--- tests.py
+++ tests.py
@@ -1,10 +1,10 @@
 import unittest
-from format_price import validation_price
+from format_price import validation_price, format_price
 
 
 class FormatPriceTestCase(unittest.TestCase):
-    def test_negetive_price(self):
-        price = validation_price(-1)
+    def test_negative_price(self):
+        price = validation_price('-1')
         self.assertIsNotNone(price)
 
     def test_input_date_with_letters(self):
@@ -12,8 +12,18 @@ class FormatPriceTestCase(unittest.TestCase):
         self.assertIsNotNone(price)
 
     def test_input_date_with_comma(self):
-        price = validation_price(0,5)
+        price = validation_price('0,5')
         self.assertIsNotNone(price)
+    
+    def test_change_price_after_formated(self):
+        price = format_price('3333333333333333333333333')
+        formated_price = '3 333 333 333 333 333 333 333 333'
+        self.assertEqual(price, formated_price)
+    
+    def test_fractional_part_formated(self):
+        price = format_price('123.000000000')
+        formated_price = '123'
+        self.assertEqual(price, formated_price)
 
 
 if __name__ == '__main__':
